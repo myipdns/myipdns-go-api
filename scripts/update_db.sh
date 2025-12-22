7889d13b115d993a6752913cc8efad527a2e42f1
XY <etmx@hotmail.com>
2025-12-22 15:27:35 +0800

    feat: add multi-language localization data files and a database update script.

--- scripts/update_db.sh
+++ scripts/update_db.sh
@@ -10,10 +10,8 @@ STATUS=$?
 if [ $STATUS -eq 0 ]; then
     echo "[$(date)] GeoIP database updated successfully."
     
-    # 2. 检查数据库文件是否真的变新了 (可选，geoipupdate 只有在有更新时才下载)
-    # 这里简单粗暴一点，只要运行成功就尝试重启 API
-    # 也可以比较文件 md5，但 MaxMind 每周才更新一次，重启一次无伤大雅
-    
+   
+
     echo "Reloading Go API service..."
     systemctl restart myip-api.service
 else
